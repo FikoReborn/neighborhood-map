7435f982f864ceb01ea122fba9a88dd060909620
Adam M. Duff <fikosan@gmail.com>
2018-08-09 20:25:49 -0400

    feat: Added working google maps api

--- src/NeighborhoodMap.js
+++ src/NeighborhoodMap.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import loadjs from 'loadjs';
 import "./App.css";
 
-class App extends Component {
+class NeighborhoodMap extends Component {
     componentDidMount() {
         loadjs('https://maps.googleapis.com/maps/api/js?key=AIzaSyCGnAvu4__n-bl-rsNch6sLTHksCDbWJGg', this.initMap)
     }
@@ -11,19 +11,18 @@ class App extends Component {
     new window.google.maps.Map(document.getElementById('map'), {
       zoom: 17,
       center: {
-        lat: 42.0776,
-        lng: -73.9529
+        lat: this.props.lat,
+        lng: this.props.lng
       }
     });
   }
 
   render() {
     return (
-      <div class="App">
         <div className="gmap" id="map" />
-      </div>  
+        
     )
   }
 }
 
-export default App;
+export default NeighborhoodMap;
