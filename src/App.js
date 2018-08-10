b9def4d0811abe009c569a7c82781a78b498c3d3
Adam M. Duff <fikosan@gmail.com>
2018-08-10 18:29:59 -0400

    feat: Filter div created

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from "react";
-import loadjs from 'loadjs';
+import loadjs from "loadjs";
+import FilterOptions from "./FilterOptions";
 import "./App.css";
 
 class App extends Component {
-    componentDidMount() {
-        loadjs('https://maps.googleapis.com/maps/api/js?key=AIzaSyCGnAvu4__n-bl-rsNch6sLTHksCDbWJGg', this.initMap)
-    }
+  componentDidMount() {
+    loadjs(
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyCGnAvu4__n-bl-rsNch6sLTHksCDbWJGg",
+      this.initMap
+    );
+  }
 
   initMap = () => {
-    new window.google.maps.Map(document.getElementById('map'), {
+    new window.google.maps.Map(document.getElementById("map"), {
       zoom: 17,
       center: {
         lat: 42.0776,
         lng: -73.9529
       }
     });
-  }
+  };
 
   render() {
     return (
-      <div class="App">
-        <div className="gmap" id="map" />
-      </div>  
-    )
+      <div className="App">
+        <FilterOptions />
+        <div className="neighborhood-map" id="map" />
+      </div>
+    );
   }
 }
 
---
+++ src/FilterOptions.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class FilterOptions extends Component {
+  render() {
+    return (
+      <div className="filter-options-container">
+        <div className="filter-options-form">
+            <input type="text" name="Filter" placeholder="Filter Locations" className="search-field" />
+            <ul className='list-locations'>
+                <li><a href="#">Test</a></li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                
+            </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FilterOptions;
