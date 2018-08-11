255e70dc3b1a14ac335d66280ff75a5226052087
Adam M. Duff <fikosan@gmail.com>
2018-08-10 23:27:44 -0400

    refactor: Styles added to map

--- src/App.js
+++ src/App.js
@@ -7,119 +7,119 @@ class App extends Component {
   state = {
     locations: [
       {
-        title: 'Bear Mountain State Park', 
+        title: "Bear Mountain State Park",
         location: {
-          lat: 41.2846, 
+          lat: 41.2846,
           lng: -74.0006
         }
       },
       {
-        title: 'Blauvelt State Park', 
+        title: "Blauvelt State Park",
         location: {
-          lat: 41.0715, 
+          lat: 41.0715,
           lng: -73.9389
         }
       },
       {
-        title: 'Clarence Fahnestock Memorial State Park',
+        title: "Clarence Fahnestock Memorial State Park",
         location: {
           lat: 41.4657,
           lng: -73.8364
         }
       },
       {
-        title: 'Clermont State Park',
+        title: "Clermont State Park",
         location: {
           lat: 42.0828,
           lng: -73.9051
         }
       },
       {
-        title: 'High Tor State Park',
+        title: "High Tor State Park",
         location: {
           lat: 41.1908,
           lng: -73.9819
         }
       },
       {
-        title: 'Hook Mountain State Park',
+        title: "Hook Mountain State Park",
         location: {
           lat: 41.1279,
           lng: -73.9123
         }
       },
       {
-        title: 'James Baird State Park',
+        title: "James Baird State Park",
         location: {
-          lat: 41.6860,
+          lat: 41.686,
           lng: -73.7917
         }
       },
       {
-        title: 'Lake Taghkanic State Park',
+        title: "Lake Taghkanic State Park",
         location: {
           lat: 42.0954,
           lng: -73.7134
         }
       },
       {
-        title: 'Mohansic State Park',
+        title: "Mohansic State Park",
         location: {
           lat: 41.2821,
           lng: -73.8082
         }
       },
       {
-        title: 'Norrie State Park',
+        title: "Norrie State Park",
         location: {
           lat: 41.8413,
           lng: -73.9404
         }
       },
       {
-        title: 'Rockland Lake State Park',
+        title: "Rockland Lake State Park",
         location: {
           lat: 41.1466,
           lng: -73.9184
         }
       },
       {
-        title: 'Stony Point State Park',
+        title: "Stony Point State Park",
         location: {
           lat: 41.2413,
           lng: 73.9735
         }
       },
       {
-        title: 'Storm King State Park',
+        title: "Storm King State Park",
         location: {
           lat: 41.4318,
           lng: -73.9973
         }
       },
       {
-        title: 'Taconic State Park',
+        title: "Taconic State Park",
         location: {
           lat: 42.1221,
           lng: -73.5154
         }
       },
       {
-        title: 'Tallman Mountain State Park',
+        title: "Tallman Mountain State Park",
         location: {
           lat: 41.0305,
-          lng: -73.9140
+          lng: -73.914
         }
       },
       {
-        title: 'Vanderbilt Mansion National Historic Site',
+        title: "Vanderbilt Mansion National Historic Site",
         location: {
           lat: 41.7963,
           lng: -73.9424
         }
       }
     ]
-  }
+  };
 
   componentDidMount() {
     loadjs(
@@ -130,11 +130,149 @@ class App extends Component {
 
   initMap = () => {
     const locations = this.state.locations;
+    // Style made by Aiziel Nazario and posted to snazzymaps.com
+    // https://snazzymaps.com/style/137900/green
+    const styles = [
+      {
+        featureType: "all",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#ffffff"
+          }
+        ]
+      },
+      {
+        featureType: "all",
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            visibility: "on"
+          },
+          {
+            color: "#3e606f"
+          },
+          {
+            weight: 2
+          },
+          {
+            gamma: 0.84
+          }
+        ]
+      },
+      {
+        featureType: "all",
+        elementType: "labels.icon",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "administrative",
+        elementType: "geometry",
+        stylers: [
+          {
+            weight: 0.6
+          },
+          {
+            color: "#313536"
+          }
+        ]
+      },
+      {
+        featureType: "landscape",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#44a688"
+          }
+        ]
+      },
+      {
+        featureType: "poi",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#13876c"
+          }
+        ]
+      },
+      {
+        featureType: "poi.attraction",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#f5e4e4"
+          },
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "poi.attraction",
+        elementType: "labels",
+        stylers: [
+          {
+            visibility: "on"
+          },
+          {
+            lightness: "14"
+          }
+        ]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#13876c"
+          },
+          {
+            visibility: "simplified"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#067372"
+          },
+          {
+            lightness: "-20"
+          }
+        ]
+      },
+      {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#357374"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#004757"
+          }
+        ]
+      }
+    ];
+
     const map = new window.google.maps.Map(document.getElementById("map"), {
       zoom: 9,
+      styles: styles,
       center: {
         lat: 41.7004,
-        lng: -73.9210
+        lng: -73.921
       }
     });
 
@@ -147,17 +285,14 @@ class App extends Component {
         title: title,
         animation: window.google.maps.Animation.DROP,
         id: i
-      })
+      });
     }
-
   };
 
   render() {
     return (
       <div className="App">
-        <FilterOptions 
-          locations={this.state.locations}
-        />
+        <FilterOptions locations={this.state.locations} />
         <div className="neighborhood-map" id="map" />
       </div>
     );
